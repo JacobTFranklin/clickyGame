807ccf48468c746b0f332c5a78bf29280b2bece7
Jacob Franklin <jacob.franklin@healthtel.net>
2019-02-11 17:13:20 -0600

    Updates

---
+++ precache-manifest.14111223cf0d124e4a9b9815347aa190.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5e1124212e913e930db",
+    "url": "/clickyGame/static/js/runtime~main.f5e11242.js"
+  },
+  {
+    "revision": "067f04274b1b3c494187",
+    "url": "/clickyGame/static/js/main.067f0427.chunk.js"
+  },
+  {
+    "revision": "b5b812a7ac6b563bd774",
+    "url": "/clickyGame/static/js/1.b5b812a7.chunk.js"
+  },
+  {
+    "revision": "067f04274b1b3c494187",
+    "url": "/clickyGame/static/css/main.95da7318.chunk.css"
+  },
+  {
+    "revision": "bf072c1073780bc520c4888b283acb16",
+    "url": "/clickyGame/index.html"
+  }
+];
